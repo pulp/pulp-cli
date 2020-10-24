5838c3a65defce662fb53614b803dedb28d8defc
Matthias Dellweg <mdellweg@redhat.com>
2020-10-24 14:02:09 +0200

    Add command to show and join tasks

--- pulpcore/cli/core_cli.py
+++ pulpcore/cli/core_cli.py
@@ -6,7 +6,7 @@ from pulpcore.cli.core.orphans import orphans
 from pulpcore.cli.core.task import task
 
 
-@main.command()
+@click.command()
 @click.pass_context
 def status(ctx: click.Context) -> None:
     pulp_ctx: PulpContext = ctx.find_object(PulpContext)
@@ -14,6 +14,8 @@ def status(ctx: click.Context) -> None:
     pulp_ctx.output_result(result)
 
 
+# Register commands with cli
+main.add_command(status)
 main.add_command(artifact)
 main.add_command(orphans)
 main.add_command(task)
