2cf216dc9f402f5ad31c930f405421878d947ce5
Matthias Dellweg <mdellweg@redhat.com>
2020-10-25 14:14:11 +0100

    Add pagination to task list

--- pulpcore/cli/core/task.py
+++ pulpcore/cli/core/task.py
@@ -1,16 +1,35 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import click
 
-from pulpcore.cli.common import PulpContext
+from pulpcore.cli.common import DEFAULT_LIMIT, BATCH_SIZE, PulpContext
 
 
 class PulpTaskContext:
     def __init__(self, pulp_ctx: PulpContext) -> None:
         self.pulp_ctx: PulpContext = pulp_ctx
 
-    def list(self, parameters: Dict[str, str]) -> Any:
-        return self.pulp_ctx.call("tasks_list", parameters=parameters)
+    def list(self, limit: int, offset: int, parameters: Dict[str, Any]) -> List[Any]:
+        count: int = -1
+        entities: List[Any] = []
+        payload: Dict[str, Any] = parameters.copy()
+        payload["offset"] = offset
+        payload["limit"] = BATCH_SIZE
+        while limit != 0:
+            if limit > BATCH_SIZE:
+                limit -= BATCH_SIZE
+            else:
+                payload["limit"] = limit
+                limit = 0
+            result: Dict[str, Any] = self.pulp_ctx.call("tasks_list", parameters=payload)
+            count = result["count"]
+            entities.extend(result["results"])
+            if result["next"] is None:
+                break
+            payload["offset"] += payload["limit"]
+        else:
+            click.echo(f"Not all {count} entries were shown.", err=True)
+        return entities
 
     def show(self, task_href: str) -> Any:
         return self.pulp_ctx.call("tasks_read", parameters={"task_href": task_href})
@@ -31,24 +50,28 @@ def task(ctx: click.Context) -> None:
 
 
 @task.command()
-@click.option("--name")
-@click.option("--name-contains", "name__contains")
-@click.option("--state")
+@click.option("--limit", default=DEFAULT_LIMIT, type=int, help="Limit the number of tasks to show.")
+@click.option("--offset", default=0, type=int, help="Skip a number of tasks to show.")
+@click.option("--name", help="List only tasks with this name.")
+@click.option("--name-contains", "name__contains", help="List only tasks whose name contains this.")
+@click.option("--state", help="List only tasks in this state.")
 @click.pass_context
-def list(ctx: click.Context, **kwargs: str) -> None:
+def list(ctx: click.Context, limit: int, offset: int, **kwargs: str) -> None:
+    """Shows a list of tasks."""
     pulp_ctx: PulpContext = ctx.find_object(PulpContext)
     task_ctx: PulpTaskContext = ctx.find_object(PulpTaskContext)
 
     params = {k: v for k, v in kwargs.items() if v is not None}
-    result = task_ctx.list(parameters=params)
+    result = task_ctx.list(limit=limit, offset=offset, parameters=params)
     pulp_ctx.output_result(result)
 
 
 @task.command()
-@click.option("--href", required=True)
-@click.option("-w", "--wait", is_flag=True)
+@click.option("--href", required=True, help="HREF of the task")
+@click.option("-w", "--wait", is_flag=True, help="Wait for the task to finish")
 @click.pass_context
 def show(ctx: click.Context, href: str, wait: bool) -> None:
+    """Shows details of a task."""
     pulp_ctx: PulpContext = ctx.find_object(PulpContext)
     task_ctx: PulpTaskContext = ctx.find_object(PulpTaskContext)
 
@@ -60,9 +83,10 @@ def show(ctx: click.Context, href: str, wait: bool) -> None:
 
 
 @task.command()
-@click.option("--href", required=True)
+@click.option("--href", required=True, help="HREF of the task")
 @click.pass_context
 def cancel(ctx: click.Context, href: str) -> None:
+    """Cancels a task and waits until the cancellation is confirmed."""
     pulp_ctx: PulpContext = ctx.find_object(PulpContext)
     task_ctx: PulpTaskContext = ctx.find_object(PulpTaskContext)
 
--- tests/scripts/test_task.sh
+++ tests/scripts/test_task.sh
@@ -4,20 +4,31 @@
 
 cleanup() {
   pulp_cli file remote destroy --name "cli_test_file_remote" || true
+  pulp_cli file remote destroy --name "cli_test_file_large_remote" || true
   pulp_cli file repository destroy --name "cli_test_file_repository" || true
   pulp_cli orphans delete || true
 }
 trap cleanup EXIT
 
 sync_task="pulp_file.app.tasks.synchronizing.synchronize"
-count=$(pulp_cli task list --name $sync_task --state canceled | jq -r .count)
+count=$(pulp_cli task list --name $sync_task --state canceled | jq -r length)
 
 pulp_cli file remote create --name "cli_test_file_remote" \
+  --url "https://fixtures.pulpproject.org/file/PULP_MANIFEST"
+pulp_cli file remote create --name "cli_test_file_large_remote" \
   --url "https://fixtures.pulpproject.org/file-large/PULP_MANIFEST"
-pulp_cli file repository create --name "cli_test_file_repository" --remote "cli_test_file_remote"
+pulp_cli file repository create --name "cli_test_file_repository" --remote "cli_test_file_large_remote"
 
+# Test canceling a task
 task=$(pulp_cli file repository sync --background --name "cli_test_file_repository" 2>&1 | grep -E -o "/pulp/api/v3/tasks/[-[:xdigit:]]*/")
 pulp_cli task cancel --href "$task"
-test "$(pulp_cli task list --name $sync_task --state canceled | jq -r .count)" -eq $((count + 1))
+test "$(pulp_cli task list --name $sync_task --state canceled | jq -r length)" -eq $((count + 1))
+test "$(pulp_cli task show --href "$task" | jq -r '.state')" = "canceled"
+
+# Test waiting for a task
+task=$(pulp_cli file repository sync --background --name "cli_test_file_repository" --remote "cli_test_file_remote" 2>&1 | grep -E -o "/pulp/api/v3/tasks/[-[:xdigit:]]*/")
+test "$(pulp_cli task show --wait --href "$task" | jq -r '.state')" = "completed"
 
 pulp_cli task list --name-contains file
+
+test "$(pulp_cli task list --limit 1 | jq -r length)" -eq 1
