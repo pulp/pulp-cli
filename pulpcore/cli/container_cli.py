2755d8c9c6278107afad96321ac776db920dc316
Matthias Dellweg <mdellweg@redhat.com>
2020-11-12 13:45:30 +0100

    Add container repository version command
    [noissue]

--- pulpcore/cli/container_cli.py
+++ pulpcore/cli/container_cli.py
@@ -1,10 +1,8 @@
 from pulpcore.cli.common import main
 from pulpcore.cli.container.repository import repository
-
-# from pulpcore.cli.container.repository_version import version
+from pulpcore.cli.container.repository_version import version
 from pulpcore.cli.container.remote import remote
 
-# from pulpcore.cli.container.publication import publication
 # from pulpcore.cli.container.distribution import distribution
 
 
@@ -14,7 +12,6 @@ def container() -> None:
 
 
 container.add_command(repository)
-# repository.add_command(version)
+repository.add_command(version)
 container.add_command(remote)
-# container.add_command(publication)
 # container.add_command(distribution)
---
+++ tests/scripts/test_container_sync.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# shellcheck source=tests/scripts/config.source
+. "$(dirname "$(realpath "$0")")/config.source"
+
+cleanup() {
+  pulp container remote destroy --name "cli_test_container_remote" || true
+  pulp container repository destroy --name "cli_test_container_repository" || true
+}
+trap cleanup EXIT
+
+# Prepare
+pulp container remote create --name "cli_test_container_remote" --url "$CONTAINER_REMOTE_URL" --upstream-name "$CONTAINER_IMAGE"
+pulp container repository create --name "cli_test_container_repository"
+
+# Test without remote (should fail)
+expect_fail pulp container repository sync --name "cli_test_container_repository"
+# Test with remote
+expect_succ pulp container repository sync --name "cli_test_container_repository" --remote "cli_test_container_remote"
+
+# Preconfigure remote
+# TBD
+# pulp container repository update --name "cli_test_container_repository" --remote "cli_test_container_remote"
+
+# Test with remote
+# expect_succ pulp container repository sync --name "cli_test_container_repository"
+# Test without remote
+# expect_succ pulp container repository sync --name "cli_test_container_repository" --remote "cli_test_container_remote"
+
+# Verify sync
+expect_succ pulp container repository version list --repository "cli_test_container_repository"
+test "$(echo "$OUTPUT" | jq -r length)" -eq 2
+
+# Delete version again
+expect_succ pulp container repository version destroy --repository "cli_test_container_repository" --version 1
