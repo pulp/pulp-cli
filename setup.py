8448fa7ee3b760fb4111f2ae5b35a748ec2703b1
Matthias Dellweg <mdellweg@redhat.com>
2020-11-13 09:50:58 +0100

    Add LICENSE and round setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,20 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="pulp-cli",
+    description="Command line interface to talk to pulpcore's REST API.",
     version="0.0.0a1.dev",
     packages=find_namespace_packages(include=["pulpcore.*"]),
     package_data={"pulpcore.cli": ["py.typed"]},
+    python_requires=">=3.6",
     install_requires=[
         "click",
         "PyYAML",
         "requests",
         "toml",
     ],
+    extras_require={
+        "pygments": ["pygments"],
+    },
     entry_points={
         "console_scripts": "pulp=pulpcore.cli:main",
         "pulp_cli.plugins": [
@@ -19,7 +24,15 @@ setup(
             "file=pulpcore.cli.file_cli",
         ],
     },
-    extras_require={
-        "pygments": ["pygments"],
-    },
+    license="GPLv2+",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Software Distribution",
+        "Typing :: Typed",
+    ],
 )
