6d609ab3f05344b2dfa452f47f7dc787ba59370c
Matthias Dellweg <mdellweg@redhat.com>
2020-12-13 12:50:01 +0100

    Add ability to manipulate group users
    [noissue]

--- tests/scripts/test_group.sh
+++ tests/scripts/test_group.sh
@@ -9,8 +9,10 @@ cleanup() {
 trap cleanup EXIT
 
 expect_succ pulp group list
-
 expect_succ pulp group create --name "cli_test_group"
 expect_succ pulp group show --name "cli_test_group"
 expect_succ pulp group list
+expect_succ pulp group user --groupname "cli_test_group" add --username "admin"
+expect_succ pulp group user --groupname "cli_test_group" add --username "AnonymousUser"
+expect_succ pulp group user --groupname "cli_test_group" remove --username "admin"
 expect_succ pulp group destroy --name "cli_test_group"
