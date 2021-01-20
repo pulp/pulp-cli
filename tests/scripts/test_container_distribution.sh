421b7f1bf30f8084dfa97e2f02740fe003c4ec02
Matthias Dellweg <mdellweg@redhat.com>
2021-01-20 12:04:28 +0100

    Make container push repositories read-only
    ContainerPushRepository is going to be changed. So this removes all
    writing action from the commands and to unblock our ci removes all tests
    for this repository type for now. We can add them back when the
    pulp_container plugin has settled.
    
    [noissue]

--- tests/scripts/test_container_distribution.sh
+++ tests/scripts/test_container_distribution.sh
@@ -6,23 +6,14 @@
 cleanup() {
   pulp container repository destroy --name "cli_test_container_repository" || true
   pulp container distribution destroy --name "cli_test_container_distro" || true
-
-  # Push repos delete their distribution automatically
-  pulp container repository --type "push" destroy --name "cli_test_container_push_repository" || true
 }
 trap cleanup EXIT
 
 expect_succ pulp container repository create --name "cli_test_container_repository"
-expect_succ pulp container repository --type "push" create --name "cli_test_container_push_repository"
 
 expect_succ pulp container distribution create --name "cli_test_container_distro" \
   --base-path "cli_test_container_distro" \
   --repository "cli_test_container_repository"
-
-expect_succ pulp container distribution create --name "cli_test_container_push_distro" \
-  --base-path "cli_test_container_push_distro" \
-  --repository "cli_test_container_push_repository" \
-  --repository-type "push"
 
 expect_succ pulp container distribution list
 expect_succ pulp container distribution show --name "cli_test_container_distro"
