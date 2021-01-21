d487a8be1aed765e3295f5ee42a09bca3ddb33fd
Marcin Kotarba <marcin.kotarba@pl.abb.com>
2021-01-21 05:09:55 -0600

    Fixed shellcheck errors
    fixes #8120

--- tests/scripts/test_file_content_bulk.sh
+++ tests/scripts/test_file_content_bulk.sh
@@ -4,8 +4,8 @@
 . "$(dirname "$(realpath "$0")")/config.source"
 
 cleanup() {
-  rm test_{1,2,3}.txt
-  rm {add,remove}_content.json
+  rm test_1.txt test_2.txt test_3.txt
+  rm add_content.json remove_content.json
   pulp file repository destroy --name "cli_test_file_repository" || true
   pulp orphans delete || true
 }
@@ -29,10 +29,8 @@ expect_succ pulp file content create --sha256 "$sha256_3" --relative-path upload
 expect_succ pulp file repository create --name "cli_test_file_repository"
 
 # Add content using JSON string
-ADD_CONTENT="[{\"sha256\":\"$sha256_1\",\"relative_path\":\"upload_test/test_1.txt\"},{\"sha256\":\"$sha256_2\",\"relative_path\":\"upload_test/test_2.txt\"},{\"sha256\":\"$sha256_3\",\"relative_path\":\"upload_test/test_3.txt\"}]"
-REMOVE_CONTENT=${ADD_CONTENT}
-expect_succ pulp file repository modify --name "cli_test_file_repository" --add-content ${ADD_CONTENT}
-expect_succ pulp file repository modify --name "cli_test_file_repository" --remove-content ${REMOVE_CONTENT}
+expect_succ pulp file repository modify --name "cli_test_file_repository" --add-content "[{\"sha256\":\"$sha256_1\",\"relative_path\":\"upload_test/test_1.txt\"},{\"sha256\":\"$sha256_2\",\"relative_path\":\"upload_test/test_2.txt\"},{\"sha256\":\"$sha256_3\",\"relative_path\":\"upload_test/test_3.txt\"}]"
+expect_succ pulp file repository modify --name "cli_test_file_repository" --remove-content "[{\"sha256\":\"$sha256_1\",\"relative_path\":\"upload_test/test_1.txt\"},{\"sha256\":\"$sha256_2\",\"relative_path\":\"upload_test/test_2.txt\"},{\"sha256\":\"$sha256_3\",\"relative_path\":\"upload_test/test_3.txt\"}]"
 
 # Add content using JSON file
 cat <<EOT >> add_content.json
