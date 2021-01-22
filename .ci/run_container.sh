99e440016f6a17cac82e95a5ca2590b37e099e53
David Davis <daviddavis@redhat.com>
2021-01-22 14:04:59 -0500

    Move to pulp/pulp container
    [noissue]

--- .ci/run_container.sh
+++ .ci/run_container.sh
@@ -17,7 +17,7 @@ then
   IMAGE_TAG="latest"
 fi
 
-"${CONTAINER_RUNTIME}" run --rm --detach --name "pulp" --volume "${BASEPATH}/settings:/etc/pulp" --publish "8080:80" "pulp/pulp-fedora31:$IMAGE_TAG"
+"${CONTAINER_RUNTIME}" run --rm --detach --name "pulp" --volume "${BASEPATH}/settings:/etc/pulp" --publish "8080:80" "pulp/pulp:$IMAGE_TAG"
 
 echo "Wait for pulp to start."
 for _ in $(seq 10)
