e7c067d291db5f046164012d02c566aa455195fd
David Davis <daviddavis@redhat.com>
2021-02-04 11:08:19 -0500

    0.3.0 release
    [noissue]

--- pulpcore/cli/common/__init__.py
+++ pulpcore/cli/common/__init__.py
@@ -8,7 +8,7 @@ import toml
 from pulpcore.cli.common.context import PulpContext
 from pulpcore.cli.common.debug import debug
 
-__version__ = "0.2.1.dev"
+__version__ = "0.3.0"
 
 
 ##############################################################################
