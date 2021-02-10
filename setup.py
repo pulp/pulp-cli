58fd0f26e51945fdedd656f5e030babf1df95690
Matthias Dellweg <mdellweg@redhat.com>
2021-02-10 20:35:09 +0100

    Bump version to 0.5.0.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name="pulp-cli",
     description="Command line interface to talk to pulpcore's REST API.",
     long_description=long_description,
-    version="0.4.0",
+    version="0.5.0.dev",
     packages=packages,
     package_data={package: ["py.typed"] for package in packages},
     python_requires=">=3.6",
