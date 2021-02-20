929882e05c7c6300a9917abb82fc0d4cca71d022
David Davis <daviddavis@redhat.com>
2021-02-20 09:26:58 -0500

    Move permissions tests to pulpcore dir
    [noissue]

--- tests/scripts/pulpcore/test_access_policy.sh
+++ tests/scripts/pulpcore/test_access_policy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # shellcheck source=tests/scripts/config.source
-. "$(dirname "$(realpath "$0")")/config.source"
+. "$(dirname "$(dirname "$(realpath "$0")")")"/config.source
 
 SAVED_ACCESS_POLICY="$(pulp access-policy show --viewset-name "tasks")"
 
