14cd8c4a61f8d8ee13f93bc6b4a00bbbf7fe5ebc
Matthias Dellweg <mdellweg@redhat.com>
2021-02-25 13:24:44 +0100

    Use the nested lookup options on version and label
    [noissue]

--- tests/scripts/pulp_file/test_label.sh
+++ tests/scripts/pulp_file/test_label.sh
@@ -12,27 +12,27 @@ trap cleanup EXIT
 
 name="cli_test_file_repo"
 expect_succ pulp file repository create --name "$name"
-expect_succ pulp file repository label set --name "$name" --key "atani" --value "hurin"
-expect_succ pulp file repository label set --name "$name" --key "ainur" --value "ulmo"
+expect_succ pulp file repository label set --repository "$name" --key "atani" --value "hurin"
+expect_succ pulp file repository label set --repository "$name" --key "ainur" --value "ulmo"
 
 expect_succ pulp file repository show --name "$name"
 test "$(echo "$OUTPUT" | jq -Src .pulp_labels)" = '{"ainur":"ulmo","atani":"hurin"}'
 
 # update a label
-expect_succ pulp file repository label set --name "$name" --key "atani" --value "beor"
+expect_succ pulp file repository label set --repository "$name" --key "atani" --value "beor"
 expect_succ pulp file repository show --name "$name"
 test "$(echo "$OUTPUT" | jq -Src .pulp_labels)" = '{"ainur":"ulmo","atani":"beor"}'
-expect_succ pulp file repository label show --name "$name" --key "atani"
+expect_succ pulp file repository label show --repository "$name" --key "atani"
 test "$OUTPUT" = "beor"
 
 # remove a label
-expect_succ pulp file repository label unset --name "$name" --key "atani"
+expect_succ pulp file repository label unset --repository "$name" --key "atani"
 expect_succ pulp file repository show --name "$name"
 test "$(echo "$OUTPUT" | jq -Src .pulp_labels)" = '{"ainur":"ulmo"}'
-expect_fail pulp file repository label show --name "$name"
+expect_fail pulp file repository label show --repository "$name"
 
 # filtering
-expect_succ pulp file repository label set --name "$name" --key "istar" --value "olorin"
+expect_succ pulp file repository label set --repository "$name" --key "istar" --value "olorin"
 expect_succ pulp file repository list --label-select "ainur"
 test "$(echo "$OUTPUT" | jq length)" -eq 1
 expect_succ pulp file repository list --label-select "ainur=ulmo"
