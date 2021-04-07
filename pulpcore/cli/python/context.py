e23a10ff3e1580a83016fe6995cb44007fec13cb
Gerrod Ubben <gerrodubben@gmail.com>
2021-04-07 17:16:28 -0400

    Added new 3.2 Python sync filter options
    fixes: #208

--- pulpcore/cli/python/context.py
+++ pulpcore/cli/python/context.py
@@ -7,6 +7,7 @@ from pulpcore.cli.common.context import (
     PulpRepositoryContext,
     PulpRepositoryVersionContext,
 )
+from pulpcore.cli.common.generic import PluginRequiredVersion as PRV
 
 
 class PulpPythonContentContext(PulpEntityContext):
@@ -57,6 +58,19 @@ class PulpPythonRemoteContext(PulpRemoteContext):
     BANDERSNATCH_ID: ClassVar[str] = "remotes_python_python_from_bandersnatch"
     UPDATE_ID = "remotes_python_python_partial_update"
     DELETE_ID = "remotes_python_python_delete"
+    field_versions = {
+        "keep_latest_packages": [PRV("python", "3.2.0")],
+        "exclude_platforms": [PRV("python", "3.2.0")],
+        "package_types": [PRV("python", "3.2.0")],
+    }
+
+    def preprocess_body(self, body: EntityDefinition) -> EntityDefinition:
+        body = super().preprocess_body(body)
+        for field in body.keys():
+            if field in self.field_versions:
+                for item in self.field_versions[field]:
+                    self.pulp_ctx.needs_plugin(*item)
+        return body
 
 
 class PulpPythonRepositoryVersionContext(PulpRepositoryVersionContext):
