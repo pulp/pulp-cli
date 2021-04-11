ccaca9c848b70c68b20a5ea80adfcc651ef37e52
Matthias Dellweg <mdellweg@redhat.com>
2021-04-11 18:57:44 +0200

    Move test script lookup into pytest plugin
    [noissue]

--- pytest_pulp_cli/__init__.py
+++ pytest_pulp_cli/__init__.py
@@ -1,7 +1,51 @@
 import os
+import subprocess
 
 import pytest
 import toml
+
+
+def pytest_collect_file(parent, path):
+    if path.ext == ".sh" and path.purebasename.startswith("test_"):
+        return ScriptFile.from_parent(parent, fspath=path)
+
+
+class ScriptFile(pytest.File):
+    # To make pytest.Function happy
+    obj = None
+
+    def collect(self):
+        name = self.fspath.purebasename[5:]
+        yield ScriptItem.from_parent(self, name=name, path=self.fspath)
+
+
+# HACK: Inherit from `pytest.Function` to be able to use the fixtures
+class ScriptItem(pytest.Function):
+    def __init__(self, path, **kwargs):
+        super().__init__(callobj=self._runscript, **kwargs)
+        self.path = path
+        self.add_marker("script")
+        if path.parts()[-3].basename == "scripts":
+            self.add_marker(path.parts()[-2].basename)
+
+    def _runscript(self, pulp_cli_env, tmp_path):
+        run = subprocess.run([self.path], cwd=tmp_path)
+        if run.returncode == 3:
+            pytest.skip("Skipped as requested by the script.")
+        if run.returncode != 0:
+            raise ScriptError(f"Script returned with exit code {run.returncode}.")
+
+    def reportinfo(self):
+        return self.fspath, 0, f"test script: {self.name}"
+
+    def repr_failure(self, excinfo):
+        if isinstance(excinfo.value, ScriptError):
+            return str(excinfo.value)
+        return super().repr_failure(excinfo)
+
+
+class ScriptError(Exception):
+    """Custom exception to mark script execution failure."""
 
 
 @pytest.fixture
