47d7762001a405776511a6500986b7d3ea7739a9
Matthias Dellweg <mdellweg@redhat.com>
2021-04-15 11:50:01 +0200

    Add debug wait-for-pulp command
    [noissue]

--- pulpcore/cli/common/debug.py
+++ pulpcore/cli/common/debug.py
@@ -17,10 +17,13 @@ def debug() -> None:
 
 @debug.command()
 @click.option("--name", required=True)
-@click.option("--min-version")
-@click.option("--max-version")
+@click.option("--min-version", help=_("Succeed only if the installed version is not smaller."))
+@click.option("--max-version", help=_("Succeed only if the installed version is smaller."))
 @pass_pulp_context
 def has_plugin(pulp_ctx: PulpContext, name: str, min_version: str, max_version: str) -> None:
+    """
+    Check whether a specific plugin is installed on the server.
+    """
     available = pulp_ctx.has_plugin(name, min_version, max_version)
     pulp_ctx.output_result(available)
     sys.exit(0 if available else 1)
--- pulpcore/cli/core/status.py
+++ pulpcore/cli/core/status.py
@@ -1,4 +1,5 @@
 import gettext
+import time
 
 import click
 
@@ -8,12 +9,33 @@ _ = gettext.gettext
 
 
 @click.command()
+@click.option("--retries", type=int, default=0, help=_("Number of retries before failing."))
+@click.option("--retry-delay", type=int, default=1, help=_("Seconds to wait between retries."))
 @pass_pulp_context
-def status(pulp_ctx: PulpContext) -> None:
+def status(pulp_ctx: PulpContext, retries: int, retry_delay: int) -> None:
     """
     Retrieve pulp status. And refresh outdated local api caches if server versions changed.
     """
-    result = pulp_ctx.call("status_read")
+    if retries < 0:
+        raise click.ClickException(_("Cannot specify a negative retry count."))
+    retries_left = retries
+    while True:
+        try:
+            result = pulp_ctx.call("status_read")
+        except click.ClickException:
+            if retries_left:
+                retries_left = retries_left - 1
+                click.echo(".", nl=False, err=True)
+                time.sleep(retry_delay)
+            else:
+                if retries:
+                    click.echo(" Failed.", err=True)
+                raise
+        else:
+            if retries:
+                click.echo(" Ready.", err=True)
+            break
+
     component_versions = {item["component"]: item["version"] for item in result.get("versions", [])}
     if component_versions != pulp_ctx.component_versions:
         click.echo("Notice: Cached api is outdated. Refreshing...", err=True)
