7f2ebd6d5bb91aa2f5ffa2e7b7f62b6afa8cadaf
Matthias Dellweg <mdellweg@redhat.com>
2021-04-30 16:56:01 +0200

    Bump version to 0.9.0.dev
    [noissue]

--- pulpcore/cli/common/__init__.py
+++ pulpcore/cli/common/__init__.py
@@ -12,7 +12,7 @@ from pulpcore.cli.common.context import PulpContext
 from pulpcore.cli.common.debug import debug
 
 _ = gettext.gettext
-__version__ = "0.8.0"
+__version__ = "0.9.0.dev"
 
 
 ##############################################################################
