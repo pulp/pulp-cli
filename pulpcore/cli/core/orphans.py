828e87b0eb0991c673e3387515cb87359d9bac0e
Matthias Dellweg <mdellweg@redhat.com>
2021-07-24 11:56:25 +0200

    Implement new orphan cleanup command
    Deprecated the old orphans delete command and added a version dependent
    switch to use the new pulpcore interface.
    
    fixes #297

--- pulpcore/cli/core/orphans.py
+++ pulpcore/cli/core/orphans.py
@@ -7,13 +7,19 @@ from pulpcore.cli.common.context import PulpContext, pass_pulp_context
 _ = gettext.gettext
 
 
-@click.group()
+@click.group(deprecated=True)
 def orphans() -> None:
+    """
+    Use 'pulp orphan' instead.
+    """
     pass
 
 
-@orphans.command()
+@orphans.command(deprecated=True)
 @pass_pulp_context
 def delete(pulp_ctx: PulpContext) -> None:
+    """
+    Use pulp 'orphan cleanup' instead.
+    """
     result = pulp_ctx.call("orphans_delete")
     pulp_ctx.output_result(result)
