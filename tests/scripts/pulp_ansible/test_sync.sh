99bb4151257534bb3cdc783f59cbe62338e89456
David Davis <daviddavis@redhat.com>
2021-08-10 15:03:39 -0400

    Set limit to prevent a bad collection from being displayed
    [noissue]

--- tests/scripts/pulp_ansible/test_sync.sh
+++ tests/scripts/pulp_ansible/test_sync.sh
@@ -56,5 +56,5 @@ expect_succ pulp ansible repository version list --repository "cli_test_ansible_
 test "$(echo "$OUTPUT" | jq -r length)" -eq 2
 expect_succ pulp ansible repository version show --repository "cli_test_ansible_repository" --version 2
 test "$(echo "$OUTPUT" | jq -r '.content_summary.present."ansible.collection_version".count')" -gt 0
-expect_succ pulp ansible content list
+expect_succ pulp ansible content list --limit 2
 test "$(echo "$OUTPUT" | jq -r length)" -gt 0
