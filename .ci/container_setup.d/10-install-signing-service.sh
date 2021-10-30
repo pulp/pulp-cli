c13b44c5a428c01e8817d0cb2a73340f956f2dd4
Matthias Dellweg <mdellweg@redhat.com>
2021-10-30 12:27:23 +0200

    Update CI
    * Added python 3.10 to the test matrix.
    * Made run_container.sh reusable for different pulp-tooling projects.
      - Added a header reminding to keep that file in sync.
      - Added a .ci/container_setup.d run-parts directory to allow project
        specific setup scripts to run after container startup.
    
    [noissue]

---
+++ .ci/container_setup.d/10-install-signing-service.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eu
+
+if pulp --base-url "http://localhost:8080" --username "admin" --password "password" debug has-plugin --name "core" --min-version 3.11
+then
+  echo "Setup a signing service"
+  "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" bash -c "cat > /root/sign_deb_release.sh" < "${BASEPATH}/../tests/assets/sign_deb_release.sh"
+  "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" bash -c "cat > /tmp/setup_signing_service.py" < "${BASEPATH}/../tests/assets/setup_signing_service.py"
+  curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-KEY-pulp-qe | "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" bash -c "cat > /tmp/GPG-KEY-pulp-qe"
+  curl -L https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-pulp-qe | "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" gpg --import
+  echo "6EDF301256480B9B801EBA3D05A5E6DA269D9D98:6:" | "${CONTAINER_RUNTIME}" exec -i "pulp-ephemeral" gpg --import-ownertrust
+  "${CONTAINER_RUNTIME}" exec "pulp-ephemeral" chmod a+x /root/sign_deb_release.sh /tmp/setup_signing_service.py
+  "${CONTAINER_RUNTIME}" exec "pulp-ephemeral" /tmp/setup_signing_service.py /root/sign_deb_release.sh /tmp/GPG-KEY-pulp-qe
+fi
