33bfbc174b6cc8e5085175d1253f0e2149bed438
Matthias Dellweg <mdellweg@redhat.com>
2021-12-12 12:40:43 +0100

    Use ID_PREFIX for operation_ids
    [noissue]

--- pulpcore/cli/file/context.py
+++ pulpcore/cli/file/context.py
@@ -1,4 +1,4 @@
-from typing import Any, ClassVar
+from typing import Any
 
 from pulpcore.cli.common.context import (
     EntityDefinition,
@@ -20,38 +20,26 @@ class PulpFileACSContext(PulpEntityContext):
     ENTITY = _("file ACS")
     ENTITIES = _("file ACSes")
     HREF = "file_file_alternate_content_source_href"
-    LIST_ID = "acs_file_file_list"
-    READ_ID = "acs_file_file_read"
-    CREATE_ID = "acs_file_file_create"
+    ID_PREFIX = "acs_file_file"
     UPDATE_ID = "acs_file_file_partial_update"
-    DELETE_ID = "acs_file_file_delete"
-    REFRESH_ID: ClassVar[str] = "acs_file_file_refresh"
 
     def refresh(self, href: str) -> Any:
-        return self.pulp_ctx.call(
-            self.REFRESH_ID,
-            parameters={self.HREF: href},
-        )
+        return self.call("refresh", parameters={self.HREF: href})
 
 
 class PulpFileContentContext(PulpContentContext):
     ENTITY = _("file content")
     ENTITIES = _("file content")
     HREF = "file_file_content_href"
-    LIST_ID = "content_file_files_list"
-    READ_ID = "content_file_files_read"
-    CREATE_ID = "content_file_files_create"
+    ID_PREFIX = "content_file_files"
 
 
 class PulpFileDistributionContext(PulpEntityContext):
     ENTITY = _("file distribution")
     ENTITIES = _("file distributions")
     HREF = "file_file_distribution_href"
-    LIST_ID = "distributions_file_file_list"
-    READ_ID = "distributions_file_file_read"
-    CREATE_ID = "distributions_file_file_create"
+    ID_PREFIX = "distributions_file_file"
     UPDATE_ID = "distributions_file_file_partial_update"
-    DELETE_ID = "distributions_file_file_delete"
     NULLABLES = {"publication", "repository"}
 
     def preprocess_body(self, body: EntityDefinition) -> EntityDefinition:
@@ -68,10 +56,7 @@ class PulpFilePublicationContext(PulpEntityContext):
     ENTITY = _("file publication")
     ENTITIES = _("file publications")
     HREF = "file_file_publication_href"
-    LIST_ID = "publications_file_file_list"
-    READ_ID = "publications_file_file_read"
-    CREATE_ID = "publications_file_file_create"
-    DELETE_ID = "publications_file_file_delete"
+    ID_PREFIX = "publications_file_file"
 
     def preprocess_body(self, body: EntityDefinition) -> EntityDefinition:
         body = super().preprocess_body(body)
@@ -86,30 +71,19 @@ class PulpFileRemoteContext(PulpRemoteContext):
     ENTITY = _("file remote")
     ENTITIES = _("file remotes")
     HREF = "file_file_remote_href"
-    LIST_ID = "remotes_file_file_list"
-    CREATE_ID = "remotes_file_file_create"
-    READ_ID = "remotes_file_file_read"
+    ID_PREFIX = "remotes_file_file"
     UPDATE_ID = "remotes_file_file_partial_update"
-    DELETE_ID = "remotes_file_file_delete"
 
 
 class PulpFileRepositoryVersionContext(PulpRepositoryVersionContext):
     HREF = "file_file_repository_version_href"
-    LIST_ID = "repositories_file_file_versions_list"
-    READ_ID = "repositories_file_file_versions_read"
-    DELETE_ID = "repositories_file_file_versions_delete"
-    REPAIR_ID = "repositories_file_file_versions_repair"
+    ID_PREFIX = "repositories_file_file_versions"
 
 
 class PulpFileRepositoryContext(PulpRepositoryContext):
     HREF = "file_file_repository_href"
-    LIST_ID = "repositories_file_file_list"
-    READ_ID = "repositories_file_file_read"
-    CREATE_ID = "repositories_file_file_create"
+    ID_PREFIX = "repositories_file_file"
     UPDATE_ID = "repositories_file_file_partial_update"
-    DELETE_ID = "repositories_file_file_delete"
-    SYNC_ID = "repositories_file_file_sync"
-    MODIFY_ID = "repositories_file_file_modify"
     VERSION_CONTEXT = PulpFileRepositoryVersionContext
     CAPABILITIES = {"pulpexport": [PluginRequirement("file")]}
 
