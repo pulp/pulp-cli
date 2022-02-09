25e36ecb7dc1d08b3f0bee1b874a3a205ee621c6
Gerrod Ubben <gerrodubben@gmail.com>
2022-02-09 14:45:12 -0500

    Fix rpm distribution update for auto-distribute
    fixes: #472

--- pulpcore/cli/rpm/distribution.py
+++ pulpcore/cli/rpm/distribution.py
@@ -26,6 +26,11 @@ repository_option = resource_option(
     default_plugin="rpm",
     default_type="rpm",
     context_table={"rpm:rpm": PulpRpmRepositoryContext},
+    help=_(
+        "Repository to be used for auto-distributing."
+        " When set, this will unset the 'publication'."
+        " Specified as '[[<plugin>:]<type>:]<name>' or as href."
+    ),
 )
 
 
@@ -50,7 +55,13 @@ filter_options = [label_select_option, base_path_option, base_path_contains_opti
 lookup_options = [href_option, name_option]
 update_options = [
     click.option("--base-path"),
-    click.option("--publication"),
+    click.option(
+        "--publication",
+        help=_(
+            "Publication to be served. This will unset the 'repository' and disable "
+            "auto-distribute."
+        ),
+    ),
     repository_option,
 ]
 create_options = update_options + [
