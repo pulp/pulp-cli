d9716265533a8d94c6648424676b4c42b8a73059
Matthias Dellweg <mdellweg@redhat.com>
2022-04-19 10:23:04 +0200

    Fix help pages tests
    [noissue]

--- pulp_cli/__init__.py
+++ pulp_cli/__init__.py
@@ -1,11 +1,15 @@
 from types import ModuleType
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import click
 import pkg_resources
 
+_main: Optional[click.Group] = None
 
-def main() -> Any:
+
+def load_plugins() -> click.Group:
+    global _main
+
     ##############################################################################
     # Load plugins
     # https://packaging.python.org/guides/creating-and-discovering-plugins/#using-package-metadata
@@ -13,8 +17,16 @@ def main() -> Any:
         entry_point.name: entry_point.load()
         for entry_point in pkg_resources.iter_entry_points("pulp_cli.plugins")
     }
-    _main: click.Group = discovered_plugins["common"].main
+    _main = discovered_plugins["common"].main
+    assert isinstance(_main, click.Group)
     for plugin in discovered_plugins.values():
         if hasattr(plugin, "mount"):
             plugin.mount(_main, discovered_plugins=discovered_plugins)
+    return _main
+
+
+def main() -> Any:
+    if _main is None:
+        load_plugins()
+        assert _main is not None
     return _main()
