0b4ca81e5441457987c3c6eaedc252a7286999e8
Matthias Dellweg <mdellweg@redhat.com>
2022-06-26 11:07:59 +0200

    Add basic body validation to openapi component
    [noissue]

---
+++ CHANGES/534.feature
@@ -0,0 +1,2 @@
+Added `pulp debug openapi schema` and `pulp debug openapi schema-names` to investigate reusable schemas in the api.
+Renamed the original `pulp debug openapi schema` command to `pulp debug openapi spec`.
--- tests/scripts/test_debug.sh
+++ tests/scripts/test_debug.sh
@@ -5,6 +5,6 @@
 
 expect_succ pulp debug has-plugin --name "core" --max-version "4.0"
 expect_succ pulp debug task-summary
-expect_succ pulp debug openapi schema
+expect_succ pulp debug openapi spec
 expect_succ pulp debug openapi operation-ids
 expect_succ pulp debug openapi operation --id tasks_list
