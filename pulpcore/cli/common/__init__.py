7468cb296a6ec963ae70748fbd8cc924be50ae62
Matthias Dellweg <mdellweg@redhat.com>
2022-07-15 17:32:00 +0200

    Bump version to 0.14.2.dev
    [noissue]

--- pulpcore/cli/common/__init__.py
+++ pulpcore/cli/common/__init__.py
@@ -18,7 +18,7 @@ from pulpcore.cli.common.context import PluginRequirement, PulpContext
 from pulpcore.cli.common.debug import debug
 from pulpcore.cli.common.i18n import get_translation
 
-__version__ = "0.14.1"
+__version__ = "0.14.2.dev"
 
 translation = get_translation(__name__)
 _ = translation.gettext
