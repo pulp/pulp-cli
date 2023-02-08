8243ae54dc5b49c18ce3e5fb469d0bf5e660fd4d
Matthias Dellweg <mdellweg@redhat.com>
2023-02-08 19:10:11 +0100

    Add pulp task summary command
    As a replacement for "pulp debug task-summary" command which is
    deprecated.
    
    fixes #625

--- tests/scripts/test_debug.sh
+++ tests/scripts/test_debug.sh
@@ -4,7 +4,6 @@
 . "$(dirname "$(realpath "$0")")"/config.source
 
 expect_succ pulp debug has-plugin --name "core" --max-version "4.0"
-expect_succ pulp debug task-summary
 expect_succ pulp debug openapi spec
 expect_succ pulp debug openapi operation-ids
 expect_succ pulp debug openapi operation --id tasks_list
