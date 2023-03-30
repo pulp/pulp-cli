fc3e67c7ce205aa3916786c06b7b7aa991f37e4c
Matthias Dellweg <mdellweg@redhat.com>
2023-03-30 13:05:24 +0200

    Add all digests to artifact lookup
    fixes #662

--- pulpcore/cli/core/artifact.py
+++ pulpcore/cli/core/artifact.py
@@ -9,6 +9,7 @@ from pulpcore.cli.common.generic import (
     chunk_size_option,
     href_option,
     list_command,
+    lookup_callback,
     pass_entity_context,
     pass_pulp_context,
     pulp_group,
@@ -26,8 +27,33 @@ def artifact(ctx: click.Context, pulp_ctx: PulpCLIContext) -> None:
     ctx.obj = PulpArtifactContext(pulp_ctx)
 
 
-filter_options = [click.option("--sha256")]
-lookup_options = [href_option]
+filter_options = [
+    click.option("--md5"),
+    click.option("--sha1"),
+    click.option("--sha224"),
+    click.option("--sha256"),
+    click.option("--sha384"),
+    click.option("--sha512"),
+]
+lookup_options = [
+    href_option,
+    click.option("--md5", callback=lookup_callback("md5", PulpArtifactContext), expose_value=False),
+    click.option(
+        "--sha1", callback=lookup_callback("sha1", PulpArtifactContext), expose_value=False
+    ),
+    click.option(
+        "--sha224", callback=lookup_callback("sha224", PulpArtifactContext), expose_value=False
+    ),
+    click.option(
+        "--sha256", callback=lookup_callback("sha256", PulpArtifactContext), expose_value=False
+    ),
+    click.option(
+        "--sha384", callback=lookup_callback("sha384", PulpArtifactContext), expose_value=False
+    ),
+    click.option(
+        "--sha512", callback=lookup_callback("sha512", PulpArtifactContext), expose_value=False
+    ),
+]
 
 artifact.add_command(list_command(decorators=filter_options))
 artifact.add_command(show_command(decorators=lookup_options))
