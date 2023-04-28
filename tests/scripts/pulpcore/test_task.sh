aea1a291aeab82ba30255676203d59fb297db78d
Matthias Dellweg <mdellweg@redhat.com>
2023-04-28 11:45:31 +0200

    Use larger repository for test cancel test
    [noissue]
    
    (cherry picked from commit e0dd582e8448a31692d38c677085705401aadaa3)

--- tests/scripts/pulpcore/test_task.sh
+++ tests/scripts/pulpcore/test_task.sh
@@ -22,29 +22,34 @@ expect_succ pulp worker list --limit 1
 worker="$(echo "$OUTPUT" | jq -r '.[0].pulp_href')"
 worker_name="$(echo "$OUTPUT" | jq -r '.[0].name')"
 
-expect_succ pulp file remote create --name "cli_test_file_remote" \
-  --url "$FILE_REMOTE_URL"
+expect_succ pulp file remote create --name "cli_test_file_remote" --url "$FILE_REMOTE_URL"
 remote_href="$(echo "$OUTPUT" | jq -r '.pulp_href')"
-expect_succ pulp file remote create --name "cli_test_file_large_remote" \
-  --url "$FILE_LARGE_REMOTE_URL"
-expect_succ pulp file repository create --name "cli_test_file_repository" --remote "cli_test_file_large_remote"
+expect_succ pulp file remote create --name "cli_test_file_large_remote" --url "$FILE_LARGE_REMOTE_URL"
+expect_succ pulp file repository create --name "cli_test_file_repository" --remote "cli_test_file_remote"
 repository_href="$(echo "$OUTPUT" | jq -r '.pulp_href')"
 
 # Test canceling a task
 if pulp debug has-plugin --name "core" --min-version "3.12.0"
 then
-  expect_succ pulp --background file repository sync --name "cli_test_file_repository"
+  expect_succ pulp --background file repository sync --name "cli_test_file_repository" --remote "cli_test_file_large_remote"
   task="$(echo "$ERROUTPUT" | grep -E -o "${PULP_API_ROOT}([-_a-zA-Z0-9]+/)?api/v3/tasks/[-[:xdigit:]]*/")"
-  expect_succ pulp task cancel --href "$task"
-  expect_succ pulp task list --name $sync_task --state canceled
-  expect_succ test "$(echo "$OUTPUT" | jq -r length)" -eq $((count + 1))
-  expect_succ pulp task show --href "$task"
-  expect_succ test "$(echo "$OUTPUT" | jq -r '.state')" = "canceled"
+  if expect_succ pulp task cancel --href "$task"
+  then
+    expect_succ pulp task list --name $sync_task --state canceled
+    expect_succ test "$(echo "$OUTPUT" | jq -r length)" -eq $((count + 1))
+    expect_succ pulp task show --href "$task"
+    expect_succ test "$(echo "$OUTPUT" | jq -r '.state')" = "canceled"
+  else
+    expect_succ pulp task list --name $sync_task --state canceled
+    expect_succ test "$(echo "$OUTPUT" | jq -r length)" -eq $((count + 0))
+    expect_succ pulp task show --href "$task"
+    expect_succ test "$(echo "$OUTPUT" | jq -r '.state')" = "completed"
+  fi
   expect_succ pulp --dry-run task cancel --all
 fi
 
 # Test waiting for a task
-expect_succ pulp --background file repository sync --name "cli_test_file_repository" --remote "cli_test_file_remote"
+expect_succ pulp --background file repository sync --name "cli_test_file_repository"
 task=$(echo "$ERROUTPUT" | grep -E -o "${PULP_API_ROOT}([-_a-zA-Z0-9]+/)?api/v3/tasks/[-[:xdigit:]]*/")
 task_uuid="${task%/}"
 task_uuid="${task_uuid##*/}"
@@ -59,7 +64,7 @@ expect_succ pulp task list --started-before "21/01/12" --started-after "22/01/06
 expect_succ pulp task list --finished-before "2021-12-01" --finished-after "2022-06-01 00:00:00"
 expect_succ pulp task list --created-resource "$created_resource"
 
-if pulp debug has-plugin --name "core" --min-version "3.22.0.dev"
+if pulp debug has-plugin --name "core" --specifier ">=3.22.0"
 then
   # New style task resource filters
   expect_succ pulp task list --reserved-resource-in "$repository_href" --reserved-resource-in "$remote_href"
@@ -91,7 +96,7 @@ expect_succ pulp task list --limit 1
 expect_succ test "$(echo "$OUTPUT" | jq -r length)" -eq 1
 
 # Test purging tasks
-if pulp debug has-plugin --name core --min-version 3.17.0
+if pulp debug has-plugin --name core --specifier ">=3.17.0"
 then
   expect_succ pulp task purge
   expect_succ pulp task purge --finished-before "2021-12-01" --state "failed"
