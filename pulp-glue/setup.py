36a6df3fe3f46f717383bf3e298c715434fb3b2c
pulpbot <pulp-infra@redhat.com>
2023-08-04 14:01:21 +0000

    Bump version: 0.21.1 → 0.21.2.dev

--- pulp-glue/setup.py
+++ pulp-glue/setup.py
@@ -26,7 +26,7 @@ setup(
     author="Pulp Team",
     author_email="pulp-list@redhat.com",
     url="https://github.com/pulp/pulp-cli",
-    version="0.21.1",
+    version="0.21.2.dev",
     packages=plugin_packages,
     package_data={"": ["py.typed"]},
     python_requires=">=3.6",
