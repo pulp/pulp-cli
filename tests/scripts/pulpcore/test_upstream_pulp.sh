6c6f9f7397a6a175987176363b22c9a6cabcb715
Dennis Kliban <dkliban@redhat.com>
2023-08-04 21:21:44 -0400

    Improves the replicate command test
    When the tests are run in pulpcore or plugin GitHub repos, there is a .netrc file present so the
    replicate task succeeds. Providing a bad API root ensures that the task always fails.
    
    [noissue]

--- tests/scripts/pulpcore/test_upstream_pulp.sh
+++ tests/scripts/pulpcore/test_upstream_pulp.sh
@@ -10,13 +10,13 @@ cleanup() {
 }
 trap cleanup EXIT
 
-expect_succ pulp upstream-pulp create --name "cli_test_upstream_pulp" --base-url "$PULP_BASE_URL" --api-root "/pulp/"
+expect_succ pulp upstream-pulp create --name "cli_test_upstream_pulp" --base-url "$PULP_BASE_URL" --api-root "/123/"
 expect_succ pulp upstream-pulp show --upstream-pulp "cli_test_upstream_pulp"
 expect_succ pulp upstream-pulp list
 expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --pulp-label-select "doesnotexist"
 
-# Without credentials there is not much we can do...
+# Without credentials and a wrong API root, there is not much we can do...
 # Also this is a real dangerous command, deleting all existing repositories.
-#expect_fail pulp upstream-pulp replicate --id "cli_test_upstream_pulp"
+expect_fail pulp upstream-pulp replicate --id "cli_test_upstream_pulp"
 
 expect_succ pulp upstream-pulp destroy --id "cli_test_upstream_pulp"
