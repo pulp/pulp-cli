59e7769de7e60f13154e1a80af022579e0989acf
Matthias Dellweg <mdellweg@redhat.com>
2024-02-20 12:10:04 +0100

    WIP

---
+++ cookiecutter/ci/hooks/post_gen_project.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import shutil
+
+REMOVE_PATHS = [
+    {% if not cookiecutter.glue -%} "CHANGES/pulp-glue{{ cookiecutter.__app_label_suffix }}", {%- endif %}
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.unlink(path)
