302c6e31f619927305a374358e1d0a520bbd45b0
Matthias Dellweg <mdellweg@redhat.com>
2024-02-20 12:10:04 +0100

    Improved cookiecutter templates
    This change will extract the actual test matrix from the existing file
    and reproduce it as part of the template. No need to configure it in
    pyproject.toml anymore.
    
    [noissue]

---
+++ cookiecutter/ci/hooks/post_gen_project.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+import os
+import shutil
+import sys
+
+REMOVE_PATHS = [
+    {% if not cookiecutter.glue -%} "CHANGES/pulp-glue{{ cookiecutter.__app_label_suffix }}", {%- endif %}
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.unlink(path)
