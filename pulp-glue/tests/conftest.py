747bb8a29058dc3a6f360c9dccf4052ffdd936fe
Matthias Dellweg <mdellweg@redhat.com>
2024-03-08 14:25:23 +0100

    Add facilities to write and run tests for pulp-glue only

---
+++ pulp-glue/tests/conftest.py
@@ -0,0 +1,30 @@
+import typing as t
+
+import pytest
+
+from pulp_glue.common.context import PulpContext
+from pulp_glue.common.openapi import BasicAuthProvider
+
+
+class PulpTestContext(PulpContext):
+    # TODO check if we can just make the base class ignore echo.
+    def echo(*args, **kwargs) -> None:
+        return
+
+
+@pytest.fixture
+def pulp_ctx(
+    request: pytest.FixtureRequest, pulp_cli_settings: t.Dict[str, t.Dict[str, t.Any]]
+) -> PulpContext:
+    verbose = request.config.getoption("verbose")
+    settings = pulp_cli_settings["cli"]
+    return PulpTestContext(
+        api_kwargs={
+            "base_url": settings["base_url"],
+            "auth_provider": BasicAuthProvider(settings.get("username"), settings.get("password")),
+            "debug_callback": lambda i, s: i <= verbose and print(s),
+        },
+        api_root=settings.get("api_root", "pulp/"),
+        background_tasks=False,
+        timeout=settings.get("timeout", 120),
+    )
--- pulp-glue/tests/test_api_quirks.py
+++ pulp-glue/tests/test_api_quirks.py
@@ -1,27 +1,18 @@
-import typing as t
 from copy import deepcopy
 
 import pytest
+
 from pulp_glue.common.context import _REGISTERED_API_QUIRKS, PulpContext
 
+pytestmark = pytest.mark.glue
 
-@pytest.mark.glue
-def test_api_quirks_idempotent(
-    pulp_cli_settings: t.Tuple[t.Any, t.Dict[str, t.Dict[str, t.Any]]]
-) -> None:
+
+def test_api_quirks_idempotent(pulp_ctx: PulpContext) -> None:
     """
     Test, that the applied api quirks can be applied twice without failing.
 
     This can let us hope they will not fail once the api is fixed upstream.
     """
-    settings = pulp_cli_settings[1]["cli"]
-    pulp_ctx = PulpContext(
-        api_kwargs={"base_url": settings["base_url"]},
-        api_root=settings.get("api_root", "pulp/"),
-        background_tasks=False,
-        timeout=settings.get("timeout", 120),
-    )
-
     assert {
         "patch_content_in_query_filters",
         "patch_field_select_filters",
