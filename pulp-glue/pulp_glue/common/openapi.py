pulp/pulp-cli

commit 954a7bd4899e56d641949f6a5f0fd2f257c26ae0

Add auth_provider to api kwargs
Users of pulp-glue can now use the provided BasicAuthProvider or
implement their own provider subclass to allow for flexible
authentication according to the openapi3 specification.

[noissue]

pulp-glue/pulp_glue/common/openapi.py
@@ -44,6 +44,47 @@ class UnsafeCallError(OpenAPIError):
     pass
 
 
+class AuthProviderBase:
+    """
+    Base class for auth providers.
+
+    This abstract base class will analyze the authentication proposals of the openapi specs.
+    Different authentication schemes should be implemented by subclasses.
+    Returned auth objects need to be compatible with `requests.auth.AuthBase`.
+    """
+
+    def basic_auth(self) -> t.Optional[t.Union[t.Tuple[str, str], requests.auth.AuthBase]]:
+        """Implement this to provide means of http basic auth."""
+        return None
+
+    def __call__(
+        self,
+        security: t.List[t.Dict[str, t.List[str]]],
+        security_schemes: t.Dict[str, t.Dict[str, t.Any]],
+    ) -> t.Optional[t.Union[t.Tuple[str, str], requests.auth.AuthBase]]:
+        for proposal in security:
+            if [security_schemes[name] for name in proposal] == [
+                {"type": "http", "scheme": "basic"}
+            ]:
+                result = self.basic_auth()
+                if result:
+                    return result
+        raise OpenAPIError(_("No suitable auth scheme found."))
+
+
+class BasicAuthProvider(AuthProviderBase):
+    """
+    Reference Implementation for AuthProviderBase providing basic auth with `username`, `password`.
+    """
+
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    def basic_auth(self) -> t.Optional[t.Union[t.Tuple[str, str], requests.auth.AuthBase]]:
+        return (self.username, self.password)
+
+
 class OpenAPI:
     """
     The abstraction Layer to interact with a server providing an openapi v3 specification.
@@ -53,8 +94,7 @@ class OpenAPI:
             served api.
         doc_path: Path of the json api doc schema relative to the `base_url`.
         headers: Dictionary of additional request headers.
-        username: Username used for basic auth.
-        password: Password used for basic auth.
+        auth_provider: Object that returns requests auth objects according to the api spec.
         cert: Client certificate used for auth.
         key: Matching key for `cert` if not already included.
         validate_certs: Whether to check server TLS certificates agains a CA.
@@ -70,8 +110,7 @@ class OpenAPI:
         base_url: str,
         doc_path: str,
         headers: t.Optional[t.Dict[str, str]] = None,
-        username: t.Optional[str] = None,
-        password: t.Optional[str] = None,
+        auth_provider: t.Optional[AuthProviderBase] = None,
         cert: t.Optional[str] = None,
         key: t.Optional[str] = None,
         validate_certs: bool = True,
@@ -90,20 +129,17 @@ class OpenAPI:
         self.safe_calls_only: bool = safe_calls_only
 
         self._session: requests.Session = requests.session()
-        if username and password:
+        if auth_provider:
             if cert or key:
-                raise OpenAPIError(_("Cannot use both username/password and cert auth."))
-            self._session.auth = (username, password)
-        elif username:
-            raise OpenAPIError(_("Password is required if username is set."))
-        elif password:
-            raise OpenAPIError(_("Username is required if password is set."))
-        elif cert and key:
-            self._session.cert = (cert, key)
-        elif cert:
-            self._session.cert = cert
-        elif key:
-            raise OpenAPIError(_("Cert is required if key is set."))
+                raise OpenAPIError(_("Cannot use both 'auth' and 'cert'."))
+            self.auth_provider = auth_provider
+        else:
+            if cert and key:
+                self._session.cert = (cert, key)
+            elif cert:
+                self._session.cert = cert
+            elif key:
+                raise OpenAPIError(_("Cert is required if key is set."))
         self._session.headers.update(
             {
                 "User-Agent": user_agent or f"Pulp-glue openapi parser ({__version__})",
@@ -550,9 +586,25 @@ class OpenAPI:
     ) -> requests.PreparedRequest:
         method_spec = path_spec[method]
         content_type, data, json, files = self.render_request_body(method_spec, body, validate_body)
+        security: t.List[t.Dict[str, t.List[str]]] = method_spec.get(
+            "security", self.api_spec.get("security", {})
+        )
+        if security and self.auth_provider:
+            auth = self.auth_provider(security, self.api_spec["components"]["securitySchemes"])
+        else:
+            # No auth required? Don't provide it.
+            # No auth_provider available? Hope for the best (should do the trick for cert auth).
+            auth = None
         request = self._session.prepare_request(
             requests.Request(
-                method, url, params=params, headers=headers, data=data, json=json, files=files
+                method,
+                url,
+                auth=auth,
+                params=params,
+                headers=headers,
+                data=data,
+                json=json,
+                files=files,
             )
         )
         if content_type:

pulpcore/cli/common/generic.py
@@ -5,6 +5,7 @@ import typing as t
 from functools import lru_cache, wraps
 
 import click
+import requests
 import schema as s
 import yaml
 from pulp_glue.common.context import (
@@ -26,6 +27,7 @@ from pulp_glue.common.context import (
     PulpRepositoryVersionContext,
 )
 from pulp_glue.common.i18n import get_translation
+from pulp_glue.common.openapi import AuthProviderBase
 
 try:
     from pygments import highlight
@@ -93,6 +95,9 @@ class PulpCLIContext(PulpContext):
         format: str,
         domain: str = "default",
     ) -> None:
+        self.username = api_kwargs.pop("username", None)
+        self.password = api_kwargs.pop("password", None)
+        api_kwargs["auth_provider"] = PulpCLIAuthProvider(pulp_ctx=self)
         super().__init__(
             api_root=api_root,
             api_kwargs=api_kwargs,
@@ -104,9 +109,6 @@ class PulpCLIContext(PulpContext):
 
     def echo(self, message: str, nl: bool = True, err: bool = False) -> None:
         click.echo(message, nl=nl, err=err)
-
-    def prompt(self, text: str, hide_input: bool = False) -> t.Any:
-        return click.prompt(text, hide_input=hide_input)
 
     def output_result(self, result: t.Any) -> None:
         """
@@ -131,6 +133,18 @@ class PulpCLIContext(PulpContext):
             raise NotImplementedError(
                 _("Format '{format}' not implemented.").format(format=self.format)
             )
+
+
+class PulpCLIAuthProvider(AuthProviderBase):
+    def __init__(self, pulp_ctx: PulpCLIContext):
+        self.pulp_ctx = pulp_ctx
+
+    def basic_auth(self) -> t.Optional[t.Union[t.Tuple[str, str], requests.auth.AuthBase]]:
+        if self.pulp_ctx.username is None:
+            self.pulp_ctx.username = click.prompt("Username")
+        if self.pulp_ctx.password is None:
+            self.pulp_ctx.password = click.prompt("Password", hide_input=True)
+        return (self.pulp_ctx.username, self.pulp_ctx.password)
 
 
 ##############################################################################

pulp-glue/pulp_glue/common/context.py
@@ -4,12 +4,13 @@ import re
 import sys
 import time
 import typing as t
+import warnings
 
 from packaging.specifiers import SpecifierSet
 from requests import HTTPError
 
 from pulp_glue.common.i18n import get_translation
-from pulp_glue.common.openapi import OpenAPI, OpenAPIError
+from pulp_glue.common.openapi import BasicAuthProvider, OpenAPI, OpenAPIError
 
 translation = get_translation(__package__)
 _ = translation.gettext
@@ -306,8 +307,19 @@ class PulpContext:
         All calls to the API should be performed via `call`.
         """
         if self._api is None:
-            if self._api_kwargs.get("username") and not self._api_kwargs.get("password"):
-                self._api_kwargs["password"] = self.prompt("password", hide_input=True)
+            if self._api_kwargs.get("username"):
+                # Deprecated for 'auth'.
+                if not self._api_kwargs.get("password"):
+                    self._api_kwargs["password"] = self.prompt("password", hide_input=True)
+                self._api_kwargs["auth_provider"] = BasicAuthProvider(
+                    self._api_kwargs.pop("username"),
+                    self._api_kwargs.pop("password", None),
+                )
+                warnings.warn(
+                    "Using 'username' and 'password' with 'PulpContext' is deprecated. "
+                    "Use an auth provider with the 'auth_provider' argument instead.",
+                    DeprecationWarning,
+                )
             try:
                 self._api = OpenAPI(
                     doc_path=f"{self._api_root}api/v3/docs/api.json", **self._api_kwargs

pulpcore/cli/ansible/content.py
@@ -47,7 +47,6 @@ def _fields_callback(ctx: click.Context, param: click.Parameter, value: Any) -> 
     if value:
         click.echo(_("Option {name} is deprecated.").format(name=param.opts[0]), err=True)
         value = tuple(value.split(","))
-        print(value)
 
     return value
 

pulpcore/cli/common/config.py
@@ -53,8 +53,8 @@ CONFIG_OPTIONS = [
             "Can be specified multiple times."
         ),
     ),
-    click.option("--username", default="", help=_("Username on pulp server")),
-    click.option("--password", default="", help=_("Password on pulp server")),
+    click.option("--username", default=None, help=_("Username on pulp server")),
+    click.option("--password", default=None, help=_("Password on pulp server")),
     click.option("--cert", default="", help=_("Path to client certificate")),
     click.option(
         "--key",
@@ -130,7 +130,7 @@ def validate_config(config: Dict[str, Any], strict: bool = False) -> bool:
     if unknown_settings:
         errors.append(_("Unknown settings: '{}'.").format("','".join(unknown_settings)))
     if strict:
-        missing_settings = set(SETTINGS) - set(config.keys())
+        missing_settings = set(SETTINGS) - set(config.keys()) - {"username", "password"}
         if missing_settings:
             errors.append(_("Missing settings: '{}'.").format("','".join(missing_settings)))
     if errors:

pulpcore/cli/common/debug.py
@@ -63,6 +63,24 @@ def spec(pulp_ctx: PulpCLIContext) -> None:
 
 
 @openapi_group.command()
+@pass_pulp_context
+def info(pulp_ctx: PulpCLIContext) -> None:
+    """
+    Print info block.
+    """
+    pulp_ctx.output_result(pulp_ctx.api.api_spec["info"])
+
+
+@openapi_group.command()
+@pass_pulp_context
+def security_schemes(pulp_ctx: PulpCLIContext) -> None:
+    """
+    Print info block.
+    """
+    pulp_ctx.output_result(pulp_ctx.api.api_spec["components"]["securitySchemes"])
+
+
+@openapi_group.command()
 @click.option("--id", "operation_id", required=True, help=_("Operation ID in openapi schema"))
 @pass_pulp_context
 def operation(pulp_ctx: PulpCLIContext, operation_id: str) -> None:

commit d8fb6cf759d29cabfff53856b36045b7ca20c19b

Only set auth_provider if a no cert is provided
Otherwise (e.g. when using cert auth), pulp_glue will error out with:

    Error: Cannot use both 'auth' and 'cert'.

Fixes: 954a7bd4899e56d641949f6a5f0fd2f257c26ae0

pulp-glue/pulp_glue/common/openapi.py
@@ -127,12 +127,12 @@ class OpenAPI:
         self.base_url: str = base_url
         self.doc_path: str = doc_path
         self.safe_calls_only: bool = safe_calls_only
+        self.auth_provider = auth_provider
 
         self._session: requests.Session = requests.session()
-        if auth_provider:
+        if self.auth_provider:
             if cert or key:
                 raise OpenAPIError(_("Cannot use both 'auth' and 'cert'."))
-            self.auth_provider = auth_provider
         else:
             if cert and key:
                 self._session.cert = (cert, key)

pulpcore/cli/common/generic.py
@@ -133,7 +133,8 @@ class PulpCLIContext(PulpContext):
     ) -> None:
         self.username = api_kwargs.pop("username", None)
         self.password = api_kwargs.pop("password", None)
-        api_kwargs["auth_provider"] = PulpCLIAuthProvider(pulp_ctx=self)
+        if not api_kwargs.get("cert"):
+            api_kwargs["auth_provider"] = PulpCLIAuthProvider(pulp_ctx=self)
         super().__init__(
             api_root=api_root,
             api_kwargs=api_kwargs,