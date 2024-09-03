6d32504876d3314d73d2ee11f801e9c09188d734
Gerrod Ubben <gerrodubben@gmail.com>
2024-09-03 10:11:22 +0200

    Temporarily disable upstream label test

--- tests/scripts/pulpcore/test_upstream_pulp.sh
+++ tests/scripts/pulpcore/test_upstream_pulp.sh
@@ -13,7 +13,8 @@ trap cleanup EXIT
 expect_succ pulp upstream-pulp create --name "cli_test_upstream_pulp" --base-url "$PULP_BASE_URL" --api-root "/123/"
 expect_succ pulp upstream-pulp show --upstream-pulp "cli_test_upstream_pulp"
 expect_succ pulp upstream-pulp list
-expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --pulp-label-select "doesnotexist"
+# TODO: Add version check for this test
+# expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --pulp-label-select "doesnotexist"
 
 # Without credentials and a wrong API root, there is not much we can do...
 # Also this is a real dangerous command, deleting all existing repositories.
