556bc078c6e76698603790f5187c1dcd4e65f0ae
Matthias Dellweg <mdellweg@redhat.com>
2024-09-09 16:35:06 +0200

    Add testing scenario for oauth2
    Add a oauth2 client credentials scenario in the tests. By letting nginx
    fake the token generation and configuring Pulp to require that token.

--- .ci/settings/settings.py
+++ .ci/settings/settings.py
@@ -1,3 +1,58 @@
+import os
+
 ALLOWED_EXPORT_PATHS = ["/tmp"]
 ANALYTICS = False
 ALLOWED_CONTENT_CHECKSUMS = ["sha1", "sha256", "sha512"]
+
+if os.environ.get("PULP_HTTPS", "false").lower() == "true":
+    AUTHENTICATION_BACKENDS = "@merge django.contrib.auth.backends.RemoteUserBackend"
+    MIDDLEWARE = "@merge django.contrib.auth.middleware.RemoteUserMiddleware"
+    REST_FRAMEWORK__DEFAULT_AUTHENTICATION_CLASSES = (
+        "@merge pulpcore.app.authentication.PulpRemoteUserAuthentication"
+    )
+    REMOTE_USER_ENVIRON_NAME = "HTTP_REMOTEUSER"
+
+if os.environ.get("PULP_OAUTH2", "false").lower() == "true":
+    assert os.environ.get("PULP_HTTPS", "false").lower() == "true"
+
+    def PulpCliFakeOauth2Authentication(*args, **kwargs):
+        # We need to lazy load this.
+        # Otherwise views may be instanciated, before this configuration is merged.
+
+        from django.contrib.auth import authenticate
+        from drf_spectacular.extensions import OpenApiAuthenticationExtension
+        from rest_framework.authentication import BaseAuthentication
+
+        class _PulpCliFakeOauth2Authentication(BaseAuthentication):
+            def authenticate(self, request):
+                auth_header = request.META.get("HTTP_AUTHORIZATION")
+                if auth_header == "Bearer DEADBEEF":
+                    return authenticate(request, remote_user="admin"), None
+                else:
+                    return None
+
+            def authenticate_header(self, request):
+                return 'Bearer realm="Pulp"'
+
+        class PulpCliFakeOauth2AuthenticationScheme(OpenApiAuthenticationExtension):
+            target_class = _PulpCliFakeOauth2Authentication
+            name = "PulpCliFakeOauth2"
+
+            def get_security_definition(self, auto_schema):
+                return {
+                    "type": "oauth2",
+                    "flows": {
+                        "clientCredentials": {
+                            "tokenUrl": "https://localhost:8080/oauth2token/",
+                            "scopes": {"api.console": "grant_access_to_pulp"},
+                        },
+                    },
+                }
+
+        return _PulpCliFakeOauth2Authentication(*args, **kwargs)
+
+    PULP_CLI_FAKE_OAUTH2_AUTHENTICATION = PulpCliFakeOauth2Authentication
+
+    REST_FRAMEWORK__DEFAULT_AUTHENTICATION_CLASSES = (
+        "@merge pulpcore.app.settings.PULP_CLI_FAKE_OAUTH2_AUTHENTICATION"
+    )
--- pulp-glue/tests/conftest.py
+++ pulp-glue/tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import typing as t
 
 import pytest
@@ -6,29 +7,27 @@ from pulp_glue.common.context import PulpContext
 from pulp_glue.common.openapi import BasicAuthProvider
 
 
-class PulpTestContext(PulpContext):
-    # TODO check if we can just make the base class ignore echo.
-    def echo(*args, **kwargs) -> None:
-        return
-
-
 @pytest.fixture
 def pulp_ctx(
     request: pytest.FixtureRequest, pulp_cli_settings: t.Dict[str, t.Dict[str, t.Any]]
 ) -> PulpContext:
+    if os.environ.get("PULP_OAUTH2", "").lower() == "true":
+        pytest.skip("Pulp-glue in isolation does not support OAuth2 atm.")
     verbose = request.config.getoption("verbose")
     settings = pulp_cli_settings["cli"].copy()
     settings["debug_callback"] = lambda i, s: i <= verbose and print(s)
-    return PulpTestContext.from_config(settings)
+    return PulpContext.from_config(settings)
 
 
 @pytest.fixture
 def fake_pulp_ctx(
     request: pytest.FixtureRequest, pulp_cli_settings: t.Dict[str, t.Dict[str, t.Any]]
 ) -> PulpContext:
+    if os.environ.get("PULP_OAUTH2", "").lower() == "true":
+        pytest.skip("Pulp-glue in isolation does not support OAuth2 atm.")
     verbose = request.config.getoption("verbose")
     settings = pulp_cli_settings["cli"]
-    return PulpTestContext(
+    return PulpContext(
         api_kwargs={
             "base_url": settings["base_url"],
             "auth_provider": (
