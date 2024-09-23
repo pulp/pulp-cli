7d2984853c3cc732f9d67b85fbd3594ce4394148
Matthias Dellweg <mdellweg@redhat.com>
2024-09-23 12:00:50 +0200

    Fix from_config messing up headers
    An extra comma creating a tuple made the function unusable.

--- pulp-glue/tests/conftest.py
+++ pulp-glue/tests/conftest.py
@@ -17,23 +17,9 @@ def pulp_ctx(
     request: pytest.FixtureRequest, pulp_cli_settings: t.Dict[str, t.Dict[str, t.Any]]
 ) -> PulpContext:
     verbose = request.config.getoption("verbose")
-    settings = pulp_cli_settings["cli"]
-    return PulpTestContext(
-        api_kwargs={
-            "base_url": settings["base_url"],
-            "auth_provider": (
-                BasicAuthProvider(settings.get("username"), settings.get("password"))
-                if "username" in settings
-                else None
-            ),
-            "cert": settings.get("cert"),
-            "key": settings.get("key"),
-            "debug_callback": lambda i, s: i <= verbose and print(s),
-        },
-        api_root=settings.get("api_root", "pulp/"),
-        background_tasks=False,
-        timeout=settings.get("timeout", 120),
-    )
+    settings = pulp_cli_settings["cli"].copy()
+    settings["debug_callback"] = lambda i, s: i <= verbose and print(s)
+    return PulpTestContext.from_config(settings)
 
 
 @pytest.fixture
