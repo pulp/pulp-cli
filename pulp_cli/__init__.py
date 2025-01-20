3186676895a730c42debf5e7a1315b27eaf6bb2c
pulpbot <pulp-infra@redhat.com>
2025-01-20 19:24:00 +0000

    Bump version: 0.30.2 → 0.30.3.dev

--- pulp_cli/__init__.py
+++ pulp_cli/__init__.py
@@ -27,7 +27,7 @@ try:
 except ImportError:
     HAS_CLICK_SHELL = False
 
-__version__ = "0.30.2"
+__version__ = "0.30.3.dev"
 translation = get_translation(__package__)
 _ = translation.gettext
 # Keep track to prevent loading plugins twice
--- pulpcore/cli/common/__init__.py
+++ pulpcore/cli/common/__init__.py
@@ -4,7 +4,7 @@ import click
 
 from pulpcore.cli.common.debug import debug
 
-__version__ = "0.30.2"
+__version__ = "0.30.3.dev"
 
 
 def mount(main: click.Group, **kwargs: t.Any) -> None:
