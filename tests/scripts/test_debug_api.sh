8f38ce393edadf7c578928b8be6d76995b6772f0
Matthias Dellweg <mdellweg@redhat.com>
2025-03-07 16:06:14 +0100

    Fix headers with special characters in config
    This adjusts the regular expression for headers to allow all characters
    in the value. It also adds the same validation to the header option and
    merges headers from config and options.

--- tests/scripts/test_debug_api.sh
+++ tests/scripts/test_debug_api.sh
@@ -5,5 +5,7 @@ set -eu
 . "$(dirname "$(realpath "$0")")"/config.source
 
 expect_succ pulp -v status
-
 echo "${ERROUTPUT}" | grep -q "^status_read : get https\?://\w.*/api/v3/status/$"
+
+expect_succ pulp -vv --header "test-header:Value with space" status
+echo "${ERROUTPUT}" | grep -q "^  test-header: Value with space$"
