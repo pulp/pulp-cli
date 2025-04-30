70fb80edb8c8a30f0da7746fea90cdaf9038e635
Gerrod Ubben <gerrodubben@gmail.com>
2025-04-30 17:20:56 -0400

    Add support for new replicate features

--- tests/scripts/pulpcore/test_upstream_pulp.sh
+++ tests/scripts/pulpcore/test_upstream_pulp.sh
@@ -14,8 +14,20 @@ trap cleanup EXIT
 expect_succ pulp upstream-pulp create --name "cli_test_upstream_pulp" --base-url "$PULP_BASE_URL" --api-root "/123/"
 expect_succ pulp upstream-pulp show --upstream-pulp "cli_test_upstream_pulp"
 expect_succ pulp upstream-pulp list
-# TODO: Add version check for this test
-# expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --pulp-label-select "doesnotexist"
+
+if pulp debug has-plugin --name "core" --specifier "<=3.62.0"
+then
+  expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --pulp-label-select "doesnotexist"
+else
+  expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --q-select "pulp_label_select=doesnotexist"
+fi
+
+if pulp debug has-plugin --name "core" --specifier ">=3.73.0"
+then
+  expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --policy "nodelete"
+  expect_succ pulp upstream-pulp update --id "cli_test_upstream_pulp" --policy "labeled"
+  expect_fail pulp upstream-pulp update --id "cli_test_upstream_pulp" --policy "not_a_policy"
+fi
 
 # Without credentials and a wrong API root, there is not much we can do...
 # Also this is a real dangerous command, deleting all existing repositories.
