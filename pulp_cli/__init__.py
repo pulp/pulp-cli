dfba2f72a080d275e18d675c7a664eaca3de12d6
pulpbot <pulp-infra@redhat.com>
2025-05-12 14:47:35 +0000

    Bump version: 0.30.4 → 0.30.5.dev

--- pulp_cli/__init__.py
+++ pulp_cli/__init__.py
@@ -27,7 +27,7 @@ try:
 except ImportError:
     HAS_CLICK_SHELL = False
 
-__version__ = "0.30.4"
+__version__ = "0.30.5.dev"
 translation = get_translation(__package__)
 _ = translation.gettext
 # Keep track to prevent loading plugins twice
