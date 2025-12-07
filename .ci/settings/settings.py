5e630bf39622718c9721a2fd6a6692ec9efa0f77
pulpbot <pulp-infra@redhat.com>
2025-12-07 00:47:34 +0000

    Update cookiecutter

--- .ci/settings/settings.py
+++ .ci/settings/settings.py
@@ -4,10 +4,7 @@ ALLOWED_EXPORT_PATHS = ["/tmp"]
 ANALYTICS = False
 ALLOWED_CONTENT_CHECKSUMS = ["sha1", "sha256", "sha512"]
 
-pulp_https = os.environ.get("PULP_HTTPS", "false").lower() == "true"
-pulp_oauth2 = os.environ.get("PULP_OAUTH2", "false").lower() == "true"
-
-if pulp_https and not pulp_oauth2:
+if os.environ.get("PULP_HTTPS", "false").lower() == "true":
     AUTHENTICATION_BACKENDS = "@merge django.contrib.auth.backends.RemoteUserBackend"
     MIDDLEWARE = "@merge django.contrib.auth.middleware.RemoteUserMiddleware"
     REST_FRAMEWORK__DEFAULT_AUTHENTICATION_CLASSES = (
@@ -15,14 +12,14 @@ if pulp_https and not pulp_oauth2:
     )
     REMOTE_USER_ENVIRON_NAME = "HTTP_REMOTEUSER"
 
-if pulp_oauth2:
-    assert pulp_https
+if os.environ.get("PULP_OAUTH2", "false").lower() == "true":
+    assert os.environ.get("PULP_HTTPS", "false").lower() == "true"
 
     def PulpCliFakeOauth2Authentication(*args, **kwargs):
         # We need to lazy load this.
         # Otherwise views may be instanciated, before this configuration is merged.
 
-        from django.contrib.auth import get_user_model
+        from django.contrib.auth import authenticate
         from drf_spectacular.extensions import OpenApiAuthenticationExtension
         from rest_framework.authentication import BaseAuthentication
 
@@ -30,7 +27,7 @@ if pulp_oauth2:
             def authenticate(self, request):
                 auth_header = request.META.get("HTTP_AUTHORIZATION")
                 if auth_header == "Bearer DEADBEEF":
-                    return get_user_model().objects.get(username="admin"), None
+                    return authenticate(request, remote_user="admin"), None
                 else:
                     return None
 
@@ -57,5 +54,5 @@ if pulp_oauth2:
     PULP_CLI_FAKE_OAUTH2_AUTHENTICATION = PulpCliFakeOauth2Authentication
 
     REST_FRAMEWORK__DEFAULT_AUTHENTICATION_CLASSES = (
-        "pulpcore.app.settings.PULP_CLI_FAKE_OAUTH2_AUTHENTICATION",
+        "@merge pulpcore.app.settings.PULP_CLI_FAKE_OAUTH2_AUTHENTICATION"
     )
