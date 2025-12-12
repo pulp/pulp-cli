5073c98e8b0951b4fa2072f713aab308090ad147
pulpbot <pulp-infra@redhat.com>
2025-12-12 15:12:04 +0000

    Release 0.36.3

--- pulp_cli/__init__.py
+++ pulp_cli/__init__.py
@@ -28,7 +28,7 @@ try:
 except ImportError:
     HAS_CLICK_SHELL = False
 
-__version__ = "0.36.3.dev"
+__version__ = "0.36.3"
 translation = get_translation(__package__)
 _ = translation.gettext
 # Keep track to prevent loading plugins twice
